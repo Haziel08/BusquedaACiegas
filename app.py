a6ad008933b0b3e13063a7b7cbec6ef1af7cb3e5
Haziel <haziel635@gmail.com>
2026-03-02 23:40:15 -0600

    Visual arreglando

--- app.py
+++ app.py
@@ -1,32 +1,9 @@
-from flask import Flask, render_template, request, jsonify
-from modelo import BuscadorCasa
-
+from flask import Flask, render_template
 app = Flask(__name__)
-buscador = BuscadorCasa()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/nodos', methods=['GET'])
-def obtener_nodos():
-    # Enviamos los nombres de las habitaciones para llenar los selectores
-    return jsonify(sorted(list(buscador.grafo.keys())))
-
-@app.route('/buscar', methods=['POST'])
-def buscar():
-    data = request.json
-    modo = data.get('modo')
-    inicio = data.get('inicio')
-    meta = data.get('meta')
-    
-    # Obtenemos todos los pasos del generador de modelo.py
-    if modo == "BFS":
-        pasos = list(buscador.bfs_paso_a_paso(inicio, meta))
-    else:
-        pasos = list(buscador.dfs_paso_a_paso(inicio, meta))
-    
-    return jsonify(pasos)
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
